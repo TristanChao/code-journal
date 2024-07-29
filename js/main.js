5c72dca0e2c46b355ae6fbfdd15197c3aec0eecb
Tristan Chao <chaot372@gmail.com>
2024-07-29 18:16:13 +0000

    added TS event listener to update url of image placeholder when the photo url input is changed

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
+'use strict';
 /* global data */
-
-const $photoUrlInput = document.querySelector(
-  '#photo-url-input',
-) as HTMLInputElement;
-const $entryImg = document.querySelector('#entry-img') as HTMLImageElement;
-
+const $photoUrlInput = document.querySelector('#photo-url-input');
+const $entryImg = document.querySelector('#entry-img');
 if (!$photoUrlInput) throw new Error('$photoUrlInput query failed');
 if (!$entryImg) throw new Error('$entryImg query failed');
-
 $photoUrlInput.addEventListener('input', () => {
   if (!$photoUrlInput.value) {
     $entryImg.setAttribute('src', '/images/placeholder-image-square.jpg');
     return;
   }
-
   $entryImg.setAttribute('src', $photoUrlInput.value);
 });
-
-if (!data) console.log('i just need to get rid of this error for now');
